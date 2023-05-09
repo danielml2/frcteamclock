48ef1d54f6fd075adac72bd6bebd442282423210
Daniel <64473184+danielml2@users.noreply.github.com>
2023-05-09 16:58:58 +0300

    Fixed counterwith the visibility change event
    - Formatted code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,166 +1,235 @@
-<head><link rel='icon' type='image/png' href='../favicon.png' /></head>
 <script>
   import { onDestroy, onMount } from "svelte";
-  import teamTimeData from "../timeData.json"
+  import teamTimeData from "../timeData.json";
 
   let displayText = "Loading..";
   let moreInfoOpen = false;
   let lastDateString = "";
-  let extraDetails = []
-  let description = ""
+  let extraDetails = [];
+  let description = "";
   let website;
   let teamNumber = 1;
   let avatar = "None";
 
-
   const updateLoop = setInterval(() => {
-      let currentDateString = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "2-digit"})
-      if(lastDateString != currentDateString) {
-          displayText = currentDateString;
-          lastDateString = currentDateString;
+    let currentDateString = new Date().toLocaleTimeString("en-GB", {
+      hour: "numeric",
+      minute: "2-digit",
+    });
+    if (lastDateString != currentDateString) {
+      displayText = currentDateString;
+      lastDateString = currentDateString;
 
-          let timeSplit = currentDateString.split(":")
-          teamNumber = Number.parseInt(timeSplit[0] + timeSplit[1]);
-          let teamData = getTeamData(teamNumber)
-          if(teamData["team_number"] != "not in TBA's API") {
-            if(teamData["avatarData"] != undefined)
-              avatar = teamData["avatarData"]
-            else
-              avatar = "None";
+      let timeSplit = currentDateString.split(":");
+      teamNumber = Number.parseInt(timeSplit[0] + timeSplit[1]);
+      let teamData = getTeamData(teamNumber);
+      if (teamData["team_number"] != "not in TBA's API") {
+        if (teamData["avatarData"] != undefined)
+          avatar = teamData["avatarData"];
+        else avatar = "None";
 
-            let seasonsPlayed = teamData["years"]
-            website = teamData["website"];
+        let seasonsPlayed = teamData["years"];
+        website = teamData["website"];
 
-            let lastSeason = seasonsPlayed[seasonsPlayed.length-1] 
-            description = `
-            From ${teamData["city"]}, ${teamData["country"]}${getLocation(teamData)}.
-            <br /> Last competed at: ${lastSeason}, Rookie season: ${teamData["rookie_year"]}`
+        let lastSeason = seasonsPlayed[seasonsPlayed.length - 1];
+        description = `
+            From ${teamData["city"]}, ${teamData["country"]}${getLocation(
+          teamData
+        )}.
+            <br /> Last competed at: ${lastSeason}, Rookie season: ${
+          teamData["rookie_year"]
+        }`;
 
-            extraDetails = Object.entries(teamData).filter((entry) => entry[0] != "avatarData")
-          } else {
-            description = "Unfortunately, I wasn't able to find this team! 😦"
-            website = undefined
-            extraDetails = undefined
-            avatar = "None"
-          }
-          displayText = teamData["nickname"] + ` (${teamData["team_number"]})`
-          
-
+        extraDetails = Object.entries(teamData).filter(
+          (entry) => entry[0] != "avatarData"
+        );
+      } else {
+        description = "Unfortunately, I wasn't able to find this team! 😦";
+        website = undefined;
+        extraDetails = undefined;
+        avatar = "None";
       }
-  }, 1000)
+      displayText = teamData["nickname"] + ` (${teamData["team_number"]})`;
+    }
+  }, 1000);
 
   function getTeamData(teamNumber) {
-      for(let index = 0; index < teamTimeData.length; index++){
-          let team = teamTimeData[index]
-          if(team["team_number"] == teamNumber)
-              return team;
-      }
-      return { team_number: "not in TBA's API", nickname: "Unknown" };
+    for (let index = 0; index < teamTimeData.length; index++) {
+      let team = teamTimeData[index];
+      if (team["team_number"] == teamNumber) return team;
+    }
+    return { team_number: "not in TBA's API", nickname: "Unknown" };
   }
 
   function capitalize(string) {
-    let words = string.split("_")
-    let finalString = ""
-    for(let i = 0; i < words.length; i++) {
-      let word = words[i]
+    let words = string.split("_");
+    let finalString = "";
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
       let lower = word.toLowerCase();
-      finalString += word.charAt(0).toUpperCase() + lower.slice(1) + " "
+      finalString += word.charAt(0).toUpperCase() + lower.slice(1) + " ";
     }
-    return finalString
-}
+    return finalString;
+  }
 
   function getLocation(teamData) {
-    if(teamData["school_name"] != null)
-      return ", " + teamData["school_name"]
-    else if(teamData["location_name"] != null)
-      return ", " + teamData["location_name"]
-    return ""
+    if (teamData["school_name"] != null) return ", " + teamData["school_name"];
+    else if (teamData["location_name"] != null)
+      return ", " + teamData["location_name"];
+    return "";
   }
 
   let onlineClockWatchers = 0;
   import { initializeApp, getApps } from "firebase/app";
-  import { getFirestore , runTransaction, doc, onSnapshot} from "firebase/firestore"
+  import {
+    getFirestore,
+    runTransaction,
+    doc,
+    onSnapshot,
+  } from "firebase/firestore";
   const firebaseConfig = {
     apiKey: "AIzaSyDlaAXrFNtkgcOMcjO4TrsBgvdTd-JOS7c",
     authDomain: "frcteamclock-counter.firebaseapp.com",
     projectId: "frcteamclock-counter",
     storageBucket: "frcteamclock-counter.appspot.com",
     messagingSenderId: "528853227081",
-    appId: "1:528853227081:web:99023a4dee858c01989198"
+    appId: "1:528853227081:web:99023a4dee858c01989198",
   };
 
-  const app = getApps().length == 0 ? initializeApp(firebaseConfig) : getApps()[0]
+  const app =
+    getApps().length == 0 ? initializeApp(firebaseConfig) : getApps()[0];
   const firestore = getFirestore(app);
 
-  
-  
-  const docRef = doc(firestore, "counters", "visitors")
+  const docRef = doc(firestore, "counters", "visitors");
+
+  let alreadyIncremented = false;
+  let alreadyDecremented = false;
+
   async function incrementCount() {
+    alreadyIncremented = true;
     runTransaction(firestore, async (transaction) => {
-      let docSnap = await transaction.get(docRef)
-      transaction.update(docRef, { count: docSnap.data().count + 1})
+      let docSnap = await transaction.get(docRef);
+      transaction.update(docRef, { count: docSnap.data().count + 1 });
       onlineClockWatchers = docSnap.data().count + 1;
-    })
+    });
   }
-  
+
   onDestroy(() => {
-    clearInterval(updateLoop)
-  })
+    clearInterval(updateLoop);
+  });
 
   let mounted = false;
   onMount(() => {
-    if(!mounted) {
-      incrementCount()
+    if (!mounted) {
       onSnapshot(docRef, (snapshot) => {
-      onlineClockWatchers = snapshot.data().count
-    })
-    window.addEventListener('beforeunload', () => {
-        fetch("/api/logout")
-    });
-    mounted = true;
+        onlineClockWatchers = snapshot.data().count;
+      });
+      mounted = true;
+      incrementCount();
     }
-    
-  })
-</script>  
+  });
 
-<div class="hero min-h-screen bg-base-200" >
-    <div class="hero-content text-center">
-      <div class="inset-0">
-        <h3 class="text-lg py-5">You're watching the clock with {onlineClockWatchers-1} other {onlineClockWatchers-1 >= 2 || onlineClockWatchers-1 == 0 ? "people" : "person"}.</h3>
-        <h2 class="text-2xl dark:text-white">The current time is {lastDateString} which coorelates to FRC Team:</h2>
-        {#if avatar != "None"}
-          <div class="flex flex-row justify-center"><img class="h-32 w-32" src={"data:image/png;base64," + avatar}/></div>
+  function updateVisibility() {
+    if (document.visibilityState == "visible") {
+      if (!alreadyIncremented) 
+      {
+         incrementCount();
+         alreadyDecremented = false;
+      }
+    } else {
+      if(!alreadyDecremented) {
+        alreadyIncremented = false;
+        fetch("/api/logout");
+        alreadyDecremented = true;
+      }
+      
+    }
+  }
+
+  function decrementCounter(event) {
+    alreadyDecremented = true;
+    fetch("/api/logout");
+    return "...";
+  }
+</script>
+
+<head><link rel="icon" type="image/png" href="../favicon.png" /></head>
+
+<svelte:window
+  on:beforeunload={decrementCounter}
+  on:visibilitychange={updateVisibility}
+/>
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="inset-0">
+      <h3 class="text-lg py-5">
+        You're watching the clock with {onlineClockWatchers - 1} other {onlineClockWatchers -
+          1 >=
+          2 || onlineClockWatchers - 1 == 0
+          ? "people"
+          : "person"}.
+      </h3>
+      <h2 class="text-2xl dark:text-white">
+        The current time is {lastDateString} which coorelates to FRC Team:
+      </h2>
+      {#if avatar != "None"}
+        <div class="flex flex-row justify-center">
+          <img class="h-32 w-32" src={"data:image/png;base64," + avatar} />
+        </div>
+      {/if}
+      <h1 class="text-5xl font-bold dark:text-white">{displayText}</h1>
+      <p class="pt-6 pb-6 max-width-md dark:text-white">
+        {@html description}
+      </p>
+      <div class="btn-group pt-15">
+        <button
+          on:click={() => (moreInfoOpen = !moreInfoOpen)}
+          class="btn btn-primary">More info</button
+        >
+        <button
+          on:click={() => {
+            location.href = `https://www.thebluealliance.com/team/${teamNumber}`;
+          }}
+          class="btn dark:bg-blue-700 bg-blue-700 dark:text-white"
+          >TBA Page</button
+        >
+        {#if website != undefined}
+          <button
+            on:click={() => {
+              location.href = `${website}`;
+            }}
+            class="btn bg-green-700 dark:bg-green-700 dark:text-white"
+            >Team Website</button
+          >
         {/if}
-        <h1 class="text-5xl font-bold dark:text-white">{displayText}</h1>
-        <p class="pt-6 pb-6 max-width-md dark:text-white">
-          {@html description} 
-        </p>
-        <div class="btn-group pt-15">
-          <button on:click={() => moreInfoOpen = !moreInfoOpen} class="btn btn-primary">More info</button>
-          <button on:click={() => {
-            location.href = `https://www.thebluealliance.com/team/${teamNumber}`
-          }} class="btn dark:bg-blue-700 bg-blue-700 dark:text-white">TBA Page</button>
-          {#if website != undefined}
-            <button on:click={() => {
-              location.href = `${website}`
-            }} class="btn bg-green-700 dark:bg-green-700 dark:text-white">Team Website</button>
-          {/if}
-          </div>
-        <div>
-        <div class="dark:text-white text-md italic pt-5 pb-5">Powered by <a class="dark:text-blue-500 text-blue-500  underline" href="https://www.thebluealliance.com">The Blue Alliance</a></div>
-        {#if moreInfoOpen && extraDetails != undefined} 
-        <div class="py-6">
-            <table class="table-fixed max-w-md bg-blue-400 dark:bg-blue-600 border-separate">
+      </div>
+      <div>
+        <div class="dark:text-white text-md italic pt-5 pb-5">
+          Powered by <a
+            class="dark:text-blue-500 text-blue-500 underline"
+            href="https://www.thebluealliance.com">The Blue Alliance</a
+          >
+        </div>
+        {#if moreInfoOpen && extraDetails != undefined}
+          <div class="py-6">
+            <table
+              class="table-fixed max-w-md bg-blue-400 dark:bg-blue-600 border-separate"
+            >
               {#each extraDetails as detail}
-                <tr class="dark:text-white"><td class="px-5 py-3 font-bold">{capitalize(detail[0])}</td><td class=""><div class="text-sm">{detail[1] == null ? "Not specified" : detail[1]}</div></td></tr>
+                <tr class="dark:text-white"
+                  ><td class="px-5 py-3 font-bold">{capitalize(detail[0])}</td
+                  ><td class=""
+                    ><div class="text-sm">
+                      {detail[1] == null ? "Not specified" : detail[1]}
+                    </div></td
+                  ></tr
+                >
               {/each}
             </table>
           </div>
         {/if}
-        
-        </div>
       </div>
     </div>
+  </div>
 </div>
-
-
